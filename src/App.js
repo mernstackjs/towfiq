6a55f65ef4fc9547c283d5796faaed40afb56969
Ahmed Abdirizaak <mernstack.js@gmail.com>
2022-04-15 17:23:26 +0200

    create web

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import Nav from "./Components/Nav";
+function App() {
+  return (
+    <div>
+      <Nav />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/Nav.js
@@ -0,0 +1,14 @@
+import React from "react";
+import AccountBalanceWalletIcon from "@mui/icons-material/AccountBalanceWallet";
+const Nav = () => {
+  return (
+    <div>
+      <button>gal</button>
+      <button>
+        <AccountBalanceWalletIcon />
+      </button>
+    </div>
+  );
+};
+
+export default Nav;
