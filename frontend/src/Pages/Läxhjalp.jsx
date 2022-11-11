85d2b34176901d2821fa7e4653bb4587acef8ea7
Ahmed Abdirizaak <mernstack.js@gmail.com>
2022-11-11 18:47:52 +0100

    new one w

--- frontend/src/Pages/Läxhjalp.jsx
+++ frontend/src/Pages/Läxhjalp.jsx
@@ -6,61 +6,124 @@ import p99 from "../images/p99.jpeg";
 import p11 from "../images/p11.jpeg";
 const Läxhjalp = () => {
   return (
-    <div className="flex  w-full  ">
-      <div className="basis-1/4 shadow-2xl p-2 ">
-        <img className="h-[200px] w-full object-cover" src={p99} alt="" />
-        <h1>
-          <span className="text-4xl font-bold flex flex-col py-6">
-            Läxhjälp
-          </span>
-          Föreningen har under året erbjudit läxhjälp till ungdomar som bor i
-          Spånga-Tensta. Under året har 40 barn regelbundet fått läxhjälp.
-          Läxhjälpen har varit öppet för alla både tjejer och killar.Barnen-och
-          ungdomarna fick hjälp med kärnämnen och vi utgick ifrån elevernas
-          behov. Många vårdnadshavare har engagerat sig och hjälpte till med
-          läxhjälpverksamheten.
-        </h1>
+    <>
+      <div className="flex-col md:hidden w-full  ">
+        <div className="basis-1/4 shadow-2xl p-2 ">
+          <img className="h-[200px] w-full object-cover" src={p99} alt="" />
+          <h1>
+            <span className="text-4xl font-bold flex flex-col py-6">
+              Läxhjälp
+            </span>
+            Föreningen har under året erbjudit läxhjälp till ungdomar som bor i
+            Spånga-Tensta. Under året har 40 barn regelbundet fått läxhjälp.
+            Läxhjälpen har varit öppet för alla både tjejer och
+            killar.Barnen-och ungdomarna fick hjälp med kärnämnen och vi utgick
+            ifrån elevernas behov. Många vårdnadshavare har engagerat sig och
+            hjälpte till med läxhjälpverksamheten.
+          </h1>
+        </div>
+        <div className="basis-1/4 shadow-2xl p-2">
+          <img className="h-[200px] w-full object-cover" src={p8} alt="" />
+          <h1>
+            <span className="text-4xl font-bold flex flex-col py-6">
+              Skollovsaktiviteter:
+            </span>
+            Föreningen har under 2021 ordnat följande aktiviteter för
+            skolungdomar: -idrottsaktiviteter för både tjejer och killar.
+          </h1>
+        </div>
+        <div className="basis-1/4 shadow-2xl p-2">
+          <img className="h-[200px] w-full object-cover" src={p6} alt="" />
+          <h1>
+            <span className="text-4xl font-bold flex flex-col py-6">
+              Slutord
+            </span>
+            Vi anser att vi har nått vårt mål under verksamhetsåret 2022. Alla
+            som har kommit till våra Verksamheter har fått göra det de älskar
+            mest, en chans att få utöva den aktiviteter vi erbjuder. Med ledare
+            på plats varje dag oavsett väder fanns det inte en enda dag utan
+            besökare på våra aktiviteter. Vi har utbildade ledare som är bosatta
+            i Spånga-Tensta. Och vi har erbjudit vår Ordinarie verksamhet till
+            dom barn som har efterfrågat det. Under skollovet har vi planerat
+            massa olika aktiviteter för ungdomar som inte bara fokuserar på
+            kultur. Exempel på de aktiviteter vi erbjudit är Capoeira, tjej
+            kollo, Parkour och mycket annat, Vi hoppas på dessa goda resultat
+            fortsätter för föreningen inom framtiden och vi kommer använda oss
+            av samma Metod under de kommande åren i våra övriga
+            verksamhetsområden.
+          </h1>
+        </div>
+        <div className="basis-1/4 shadow-2xl p-2">
+          <img className="h-[200px] w-full object-cover" src={p11} alt="" />
+          <h1>
+            <span className="text-4xl font-bold flex flex-col py-6">
+              Föreningen planerar följande aktiviteter för år 2022/2023
+            </span>
+            Läxhjälp för ungdomar i Spånga-Tensta  stöd och ordna
+            fritidsaktiviteter för unga tjejer  ordna idrottsaktiviteter för
+            tjejer och kvinnor
+          </h1>
+        </div>
       </div>
-      <div className="basis-1/4 shadow-2xl p-2">
-        <img className="h-[200px] w-full object-cover" src={p8} alt="" />
-        <h1>
-          <span className="text-4xl font-bold flex flex-col py-6">
-            Skollovsaktiviteter:
-          </span>
-          Föreningen har under 2021 ordnat följande aktiviteter för
-          skolungdomar: -idrottsaktiviteter för både tjejer och killar.
-        </h1>
+      <div className="hidden md:flex w-full  ">
+        <div className="basis-1/4 shadow-2xl p-2 ">
+          <img className="h-[200px] w-full object-cover" src={p99} alt="" />
+          <h1>
+            <span className="text-4xl font-bold flex flex-col py-6">
+              Läxhjälp
+            </span>
+            Föreningen har under året erbjudit läxhjälp till ungdomar som bor i
+            Spånga-Tensta. Under året har 40 barn regelbundet fått läxhjälp.
+            Läxhjälpen har varit öppet för alla både tjejer och
+            killar.Barnen-och ungdomarna fick hjälp med kärnämnen och vi utgick
+            ifrån elevernas behov. Många vårdnadshavare har engagerat sig och
+            hjälpte till med läxhjälpverksamheten.
+          </h1>
+        </div>
+        <div className="basis-1/4 shadow-2xl p-2">
+          <img className="h-[200px] w-full object-cover" src={p8} alt="" />
+          <h1>
+            <span className="text-4xl font-bold flex flex-col py-6">
+              Skollovsaktiviteter:
+            </span>
+            Föreningen har under 2021 ordnat följande aktiviteter för
+            skolungdomar: -idrottsaktiviteter för både tjejer och killar.
+          </h1>
+        </div>
+        <div className="basis-1/4 shadow-2xl p-2">
+          <img className="h-[200px] w-full object-cover" src={p6} alt="" />
+          <h1>
+            <span className="text-4xl font-bold flex flex-col py-6">
+              Slutord
+            </span>
+            Vi anser att vi har nått vårt mål under verksamhetsåret 2022. Alla
+            som har kommit till våra Verksamheter har fått göra det de älskar
+            mest, en chans att få utöva den aktiviteter vi erbjuder. Med ledare
+            på plats varje dag oavsett väder fanns det inte en enda dag utan
+            besökare på våra aktiviteter. Vi har utbildade ledare som är bosatta
+            i Spånga-Tensta. Och vi har erbjudit vår Ordinarie verksamhet till
+            dom barn som har efterfrågat det. Under skollovet har vi planerat
+            massa olika aktiviteter för ungdomar som inte bara fokuserar på
+            kultur. Exempel på de aktiviteter vi erbjudit är Capoeira, tjej
+            kollo, Parkour och mycket annat, Vi hoppas på dessa goda resultat
+            fortsätter för föreningen inom framtiden och vi kommer använda oss
+            av samma Metod under de kommande åren i våra övriga
+            verksamhetsområden.
+          </h1>
+        </div>
+        <div className="basis-1/4 shadow-2xl p-2">
+          <img className="h-[200px] w-full object-cover" src={p11} alt="" />
+          <h1>
+            <span className="text-4xl font-bold flex flex-col py-6">
+              Föreningen planerar följande aktiviteter för år 2022/2023
+            </span>
+            Läxhjälp för ungdomar i Spånga-Tensta  stöd och ordna
+            fritidsaktiviteter för unga tjejer  ordna idrottsaktiviteter för
+            tjejer och kvinnor
+          </h1>
+        </div>
       </div>
-      <div className="basis-1/4 shadow-2xl p-2">
-        <img className="h-[200px] w-full object-cover" src={p6} alt="" />
-        <h1>
-          <span className="text-4xl font-bold flex flex-col py-6">Slutord</span>
-          Vi anser att vi har nått vårt mål under verksamhetsåret 2022. Alla som
-          har kommit till våra Verksamheter har fått göra det de älskar mest, en
-          chans att få utöva den aktiviteter vi erbjuder. Med ledare på plats
-          varje dag oavsett väder fanns det inte en enda dag utan besökare på
-          våra aktiviteter. Vi har utbildade ledare som är bosatta i
-          Spånga-Tensta. Och vi har erbjudit vår Ordinarie verksamhet till dom
-          barn som har efterfrågat det. Under skollovet har vi planerat massa
-          olika aktiviteter för ungdomar som inte bara fokuserar på kultur.
-          Exempel på de aktiviteter vi erbjudit är Capoeira, tjej kollo, Parkour
-          och mycket annat, Vi hoppas på dessa goda resultat fortsätter för
-          föreningen inom framtiden och vi kommer använda oss av samma Metod
-          under de kommande åren i våra övriga verksamhetsområden.
-        </h1>
-      </div>
-      <div className="basis-1/4 shadow-2xl p-2">
-        <img className="h-[200px] w-full object-cover" src={p11} alt="" />
-        <h1>
-          <span className="text-4xl font-bold flex flex-col py-6">
-            Föreningen planerar följande aktiviteter för år 2022/2023
-          </span>
-          Läxhjälp för ungdomar i Spånga-Tensta  stöd och ordna
-          fritidsaktiviteter för unga tjejer  ordna idrottsaktiviteter för
-          tjejer och kvinnor
-        </h1>
-      </div>
-    </div>
+    </>
   );
 };
 
